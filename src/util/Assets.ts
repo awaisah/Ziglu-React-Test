9c9db0281b3c3cc4ea81500eae3dd8a7c86668b0
Awais Hussain <awais.hussain@covvi.com>
2022-01-01 11:26:10 +0000

    Added initial code

---
+++ src/util/Assets.ts
@@ -0,0 +1,8 @@
+// Indices correspond to the incides of the asset in RapidAPI GetCoin API to
+// request a price.
+export enum Asset {
+    USD = 0,
+    BTC = 1,
+    ETH = 2,
+    ADA = 9,
+  };
---
+++ src/util/bank.ts
@@ -0,0 +1,39 @@
+import { Asset } from './Assets';
+
+export class Bank {
+  private accountBalances: Map<Asset, number>;
+
+  constructor() {
+    this.accountBalances = new Map();
+    this.accountBalances.set(Asset.USD, 1000.00);
+  }
+
+  public get assetAccounts(): Asset[] {
+    return Array.from(this.accountBalances.keys());
+  }
+
+  public getBalance(asset: Asset) {
+    return this.accountBalances.get(asset) || 0.0;
+  }
+
+  public get balances(): Map<Asset, number> {
+    return new Map(this.accountBalances);
+  }
+
+  public setBalance(asset: Asset, newBalance: number) {
+    if (newBalance < 0) {
+      throw new Error("Trying to set balance to an overdrawn value - we don't support overdrafts!");
+    }
+
+    this.accountBalances.set(asset, newBalance);
+  }
+
+  public adjustBalance(asset: Asset, adjustment: number) {
+    const currentBalance = this.getBalance(asset);
+    if (adjustment < 0 && Math.abs(adjustment) > currentBalance) {
+      throw new Error('Insufficient balance for asset to perform adjustment');
+    }
+
+    this.setBalance(asset, this.getBalance(asset) + adjustment);
+  }
+}
